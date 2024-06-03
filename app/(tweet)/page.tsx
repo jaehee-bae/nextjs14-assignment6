582b7b8175d68a8c4830362485438912086ade8f
jaehee-bae <hjaeb7777@gmail.com>
2024-06-03 23:40:49 +0900

    chore: 디렉토리 변경

--- app/(tweet)/page.tsx
+++
@@ -1,9 +0,0 @@
-import TweetList from "@/components/tweet-list";
-import { getTweets } from "./actions";
-
-export default async function Home() {
-  const initialTweets = await getTweets(1);
-  return (
-      <TweetList initialTweets={initialTweets}></TweetList>
-  );
-}
